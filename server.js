822e4a354d6e552521fa224c68b0cbb82e5f0762
Mark Chapman <mark.chapman@gmail.com>
2019-01-13 15:45:05 +0000

    simple server added

---
+++ server.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = process.argv[2] || 3003;
+
+const map = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword'
+};
+
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `./public${parsedUrl.pathname}`;
+  // don't let people get root access
+  pathname = pathname.replace(/^(\.)+/, '.');
+  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext || '.html';
+  // maps file extention to MIME typere
+
+  fs.access(pathname, function (err) {
+    if(err) {
+      // if the file is not found, send 404.html
+      res.statusCode = 404;
+      pathname = './public/404.html';
+    }
+
+    // if is a directory search for index file matching the extention
+    if (fs.statSync(pathname).isDirectory()) pathname += 'index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+
+}).listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
