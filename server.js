e436a6591504ef04d5d39b9c25f1b0d695ce0fb5
Mark Chapman <mark.chapman@gmail.com>
2025-12-19 13:54:09 +0000

    Get the SSL headers up to A+

--- server.js
+++ server.js
@@ -39,6 +39,8 @@ http.createServer(function (req, res) {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Permissions-Policy', 'interest-cohort=()');
+  res.setHeader('Referrer-Policy', 'no-referrer');
 
   // Handle dodgy privacy url in shipped apps (plait-mobile)
   if (req.url.indexOf('privacy.html') > -1) {
